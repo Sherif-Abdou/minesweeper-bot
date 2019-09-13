9176057eecad2b98bbd0547ad50aa931eadd54eb
Sherif Abdou <sherif_abdou@outlook.com>
2019-09-12 17:29:07 -0700

    Run almost works

--- src/Bot.kt
+++ src/Bot.kt
@@ -1,17 +1,50 @@
 class Bot(var board: Board) {
 	fun run() {
-		mineGiven()
+		for (i in 0..2) {
+			val mined = mineGiven()
+			mined.forEach { block: Block -> lookForMarked(block) }
+		}
 	}
 
-	private fun mineGiven() {
+	fun mineGiven(): MutableList<Block> {
 		val borderBlocks = board.findBorderBlocks()
+		val minedBlocks = mutableListOf<Block>()
 		for (block in borderBlocks) {
 			val amountOfMines = board.borderNumber(block.x, block.y)
 			val invisibleNeighbors = board.findNeighboringBlocks(block.x, block.y).filter { neighbor -> !neighbor.isVisible }
 			if (amountOfMines == invisibleNeighbors.size) {
 				for (neighbor in invisibleNeighbors) {
 					neighbor.isFlagged = true
+					minedBlocks.add(neighbor)
 				}
+			}
+		}
+		return minedBlocks
+	}
+
+	fun lookForMarked(block: Block) {
+		val visibleNeighbors = board.findNeighboringBlocks(block.x, block.y).filter { neighbor -> neighbor.isVisible }
+		for (neigbhor in visibleNeighbors) {
+			checkAround(neigbhor)
+		}
+	}
+
+	private fun checkAround(block: Block) {
+		val mineNumbers = board.borderNumber(block.x, block.y)
+		val flagged = board.findNeighboringBlocks(block.x, block.y).filter { neighbor -> neighbor.isFlagged }
+		if (mineNumbers == flagged.size) {
+			val unmined = board.findNeighboringBlocks(block.x, block.y).filter { neighbor -> !neighbor.isFlagged && !neighbor.isVisible }
+			for (umine in unmined) {
+				board.mineBlock(umine.x, umine.y)
+			}
+		}
+	}
+
+	fun mineSurrounding(block: Block) {
+		val invisibleNeighbors = board.findNeighboringBlocks(block.x, block.y).filter { neighbor -> !neighbor.isVisible }
+		for (neigbhor in invisibleNeighbors) {
+			if (!neigbhor.isFlagged) {
+				neigbhor.isVisible = true
 			}
 		}
 	}
--- tests/BotTests.kt
+++ tests/BotTests.kt
@@ -2,10 +2,39 @@ fun testMineGiven() {
 	val board = Board(8, 8)
 	board.mineBlock(0, 0)
 	val bot = Bot(board)
-	bot.run()
+	bot.mineGiven()
 	println(board.toString())
 }
 
+fun testMineSurrounding() {
+	val board = Board(8, 8)
+	board.mineBlock(0, 0)
+	val bot = Bot(board)
+	bot.mineGiven()
+	bot.mineSurrounding(board.map[2][1])
+	println(board.toString())
+}
+
+fun testMineAround() {
+	val board = Board(8, 8)
+	board.mineBlock(0, 0)
+	val bot = Bot(board)
+	bot.mineGiven()
+	bot.lookForMarked(board.map[2][2])
+	println(board.toString())
+}
+
+fun testRun() {
+	val board = Board(8, 8)
+	board.mineBlock(0, 0)
+	val bot = Bot(board)
+	bot.run()
+	println(board)
+}
+
 fun main() {
-	testMineGiven()
+//	testMineGiven()
+//	testMineSurrounding()
+//	testMineAround()
+	testRun()
 }
